a0d92973ad475248394318eff00ac80048ae34df
nachopalmieri <nachopalmieri99@gmail.com>
2023-08-23 13:37:00 -0300

    change site_id

--- inmobiliaria_project/inmobiliaria/settings.py
+++ inmobiliaria_project/inmobiliaria/settings.py
@@ -143,7 +143,7 @@ AUTHENTICATION_BACKENDS = (
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
-SITE_ID = 2
+SITE_ID = 5
 LOGIN_REDIRECT_URL = 'home'
 #LOGOUT_REDIRECT_URL = 'home'
 LOGIN_URL = 'login'
