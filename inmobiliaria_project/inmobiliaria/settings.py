dda4651511477e3f3a1ece6a20ec9c3284af292f
nachopalmieri <nachopalmieri99@gmail.com>
2023-08-29 13:42:07 -0300

    problema crispy_boostrap

--- inmobiliaria_project/inmobiliaria/settings.py
+++ inmobiliaria_project/inmobiliaria/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'crispy_bootstrap5',
+    #'crispy_bootstrap5',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
