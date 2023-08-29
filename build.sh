af4412f9c50ae12fb012c57708a098c394f23e91
nachopalmieri <nachopalmieri99@gmail.com>
2023-08-29 15:37:53 -0300

    cambio admin

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ pip install -r ./requirements.txt
 cd $(dirname $(find . | grep manage.py$))
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --username admin --email "nachopalmieri99@gmail.com" --noinput || true
+python manage.py createsuperuser --username admin123 --email "nachopalmieri99@gmail.com" --noinput || true
