a33f211ccd585909afd08ecb8ff38b883854bc0b
nachopalmieri <nachopalmieri99@gmail.com>
2023-08-29 12:22:14 -0300

    remove crispy_boostrap4

--- inmobiliaria_project/inmobiliaria/settings.py
+++ inmobiliaria_project/inmobiliaria/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'crispy_forms',
-    'crispy_bootstrap4',
+    #'crispy_bootstrap4',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
